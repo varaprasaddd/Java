661f9b91ae2847cef15d845f7e6d487626cf6ec3
varaprasaddd <telagamsettivaraprasadreddy@gmail.com>
2026-08-30 21:12:20 +0530

    Add files via upload

--- java/datatype.java
+++ java/datatype.java
@@ -1,6 +1,29 @@
+/*
 public class datatype {
     public static void main(String[] args) {
         System.out.println("Hello from datatype class!");
     }
-}
+}*/
 
+// PrimitiveTypes.java
+public class datatype {
+    public static void main(String[] args) {
+        byte b = 10;
+        short s = 200;
+        int i = 3000;
+        long l = 40000L;
+        float f = 5.5f;
+        double d = 123.456;
+        char c = 'J';
+        boolean flag = true;
+
+        System.out.println("Byte: " + b);
+        System.out.println("Short: " + s);
+        System.out.println("Int: " + i);
+        System.out.println("Long: " + l);
+        System.out.println("Float: " + f);
+        System.out.println("Double: " + d);
+        System.out.println("Char: " + c);
+        System.out.println("Boolean: " + flag);
+    }
+}
